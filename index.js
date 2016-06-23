04a39098375016b8b963c5f05924375d9dfeaf20
Nick <handnf@gmail.com>
2016-06-22 22:32:04 -0700

    search and some fixes

--- index.js
+++ index.js
@@ -44,10 +44,10 @@ app.get('/search', function(request, response) {
 app.get('/api/v1/campaigns', function(request, response) {
     name = request.params.name || "";
     pg.connect(process.env.DATABASE_URL, function(err, client, done) {
-        console.log("select * from user_redirects WHERE campaign = '"+name+"'");
-        client.query("select * from user_redirects WHERE campaign = '"+name+"'", function(err, res) {
+        console.log("select * from user_redirects WHERE campaign LIKE '%"+name+"'%");
+        client.query("select * from user_redirects WHERE campaign LIKE '%"+name+"%'", function(err, res) {
             console.error(err);
-            response.send({results: res.rows});
+            response.send({results: res.rows, err: err});
         });
     }); 
 });
@@ -58,7 +58,7 @@ app.post('/api/v1/campaigns', function(request, response) {
     var url = request.body.url;
     pg.connect(process.env.DATABASE_URL, function(err, client, done) {
         client.query("insert into user_redirects (campaign, url) values ('"+campaignName+"', '"+url+"')", function(err, res) {
-            response.send({res: 200});
+            response.send({res: 200, err: err});
         });
     });     
 });
@@ -75,7 +75,7 @@ app.get('/api/v1/resources', function(request, response) {
         client.query("select * from resources WHERE tags LIKE '%"+tag+"%' OR categories LIKE '%"+category+"%' OR title LIKE '%"+name+"%' ORDER BY clicks DESC", function(err, res) {
             console.error(err);
             console.log(res);
-            response.send({results: res.rows});
+            response.send({results: res.rows, err: err});
         });
     }); 
 });
@@ -113,7 +113,7 @@ app.get('/:campaign', function(request, response) {
     }      
     // Valid
     pg.connect(process.env.DATABASE_URL, function(err, client, done) {
-        client.query("insert into user_redirects (campaign, url) values ('"+campaignName+"', '"+url+"')", function(err, res) {
+        client.query("select * from user_redirects where name='"+campaign+"' limit 1", function(err, res) {
             res.writeHead(302, {'Location': res.rows[0].url});
             res.end();
         });
