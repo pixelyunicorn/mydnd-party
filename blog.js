105311af0fbc29ea2ee8eba894a95f9db2c50b69
Nick <handnf@gmail.com>
2018-07-23 20:51:25 -0700

    Updates blog

--- blog.js
+++ blog.js
@@ -6,7 +6,8 @@ const blogposts = {
         author: 'Nick Felker',
         published: '2018-08-01T12:00:00-07:00',
         updated: '2018-08-01T12:00:00-07:00',
-        summary: `On the periodic table, lead is among the most dense metals. Lead as a material        has been around in our society for millenia. Since the beginning of civilization,
+        summary: `On the periodic table, lead is among the most dense metals. Lead as a material
+        has been around in our society for millenia. Since the beginning of civilization,
         it has been used for cosmetics, fishing, and currency. Plumbing derives from the use of
         lead for the pipes based on the Latin name for lead, plumbum.`,
         filepath: 'blog/power-of-lead.html',
